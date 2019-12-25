690307c56b9e20cf90354c729f2500f484ebec21
xiaozhenzhen <1506678296@qq.com>
2019-12-25 21:40:51 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,14 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-self-closing": "off",
+    "vue/no-parsing-error": [
+      2,
+      {
+        "x-invalid-end-tag": false
+      }
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
---
+++ src/views/Login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login">
+    <Card class="login-card">
+      <p slot="title"><Icon type="md-cloud-done" /> 请登录</p>
+      <Form ref="loginForm" :model="loginForm" :rules="loginRules" inline>
+        <FormItem prop="username">
+          <Input
+            type="text"
+            size="large"
+            v-model="loginForm.username"
+            placeholder="username"
+            clearable
+          >
+            <Icon type="ios-person-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input
+            size="large"
+            type="password"
+            v-model="loginForm.password"
+            placeholder="password"
+            clearable
+          >
+            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <br />
+        <FormItem>
+          <Button
+            type="info"
+            shape="circle"
+            @click="submitLogin('loginForm')"
+            icon="ios-radio-outline"
+            >登录</Button
+          >
+        </FormItem>
+      </Form>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginRules: {
+        username: [
+          {
+            required: true,
+            message: "请填写用户名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请填写密码",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "密码长度不能少于6位",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitLogin(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.$Message.success("登录成功!");
+          this.$router.push("/home");
+        } else {
+          this.$Message.error("未知错误!");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  padding-top: 250px;
+  background-image: url(../assets/images/login.jpeg);
+  .login-card {
+    width: 400px;
+    margin: 0 auto;
+  }
+}
+</style>
